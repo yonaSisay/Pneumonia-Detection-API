1b6206639807b21bfd22ba437002f1ba7fdc33b8
yonasisay <yonimelkamu357@gmail.com>
2024-05-17 19:15:49 +0300

    fina commit

--- main.py
+++ main.py
@@ -90,6 +90,6 @@ async def predict_url(image_url: ImageUrl):
         raise HTTPException(status_code=500, detail="An error occurred during prediction.")
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
